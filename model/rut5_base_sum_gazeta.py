529d65465ac6dea2f31aecafb07f7b36f0fe193e
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2023-11-11 18:32:12 +0600

    fixes

--- model/rut5_base_sum_gazeta.py
+++ model/rut5_base_sum_gazeta.py
@@ -3,17 +3,16 @@ from pathlib import Path
 from transformers import T5ForConditionalGeneration
 
 
-if __name__ == '__main__':
-    tokenize_path = Path('nlp/tokenize_sum')
-    model_name = 'IlyaGusev/rut5_base_sum_gazeta'
-    model_path = Path('nlp\model_sum')
+tokenize_path = Path('nlp/tokenize_sum')
+model_name = 'IlyaGusev/rut5_base_sum_gazeta'
+model_path = Path('nlp\model_sum')
 
-    if not (tokenize_path / 'vocab.txt').exists():
-        AutoTokenizer.from_pretrained(model_name).save_pretrained(str(tokenize_path))
+if not (tokenize_path / 'vocab.txt').exists():
+    AutoTokenizer.from_pretrained(model_name).save_pretrained(str(tokenize_path))
 
-    tokenizer_sum = AutoTokenizer.from_pretrained(str(tokenize_path)) 
+tokenizer_sum = AutoTokenizer.from_pretrained(str(tokenize_path)) 
 
-    if not (model_path / 'model_sum.safetensors').exists():
-        T5ForConditionalGeneration.from_pretrained(model_name).save_pretrained(str(model_path))
+if not (model_path / 'model_sum.safetensors').exists():
+    T5ForConditionalGeneration.from_pretrained(model_name).save_pretrained(str(model_path))
 
-    model_sum = T5ForConditionalGeneration.from_pretrained(str(model_path))
+model_sum = T5ForConditionalGeneration.from_pretrained(str(model_path))
