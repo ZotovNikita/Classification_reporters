ecea09c96ff4d082b8e3a8258cf9444d1e52f0ab
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2023-11-12 05:50:34 +0600

    fix

--- app.py
+++ app.py
@@ -4,9 +4,44 @@ from game.streamlit_app import game
 from exam import exam
 from cards import cards
 from theory import theory
-from game.game_scenes_chapter_1 import test3, test1, test2
+
 
 def main():
+
+    with st.sidebar:
+        selected = option_menu(
+            'Меню',
+            ['К Стёпе', 'Ментор', 'Карточки', 'Тестирование'],
+            icons=['controller', 'credit-card-2-front', 'bi-book', 'patch-question'],
+            menu_icon='cast',
+            default_index=1,
+        )
+
+    match selected:
+        case 'К Стёпе':
+            game()
+        case 'Ментор':
+            theory()
+        case 'Карточки':
+            cards()
+        case 'Тестирование':
+            exam()
+
+
+if __name__ == '__main__':
+    st.set_page_config(layout='wide')
+    remove_padding_css = """
+        .block-container {
+        padding: 2rem 2rem;
+        }
+        """
+    st.markdown(
+        "<style>"
+        + remove_padding_css
+        + "</styles>",
+        unsafe_allow_html=True,
+    )
+    st.expander("foo")
     st.markdown(
         """
         <style>
@@ -16,26 +51,4 @@ def main():
         """,
         unsafe_allow_html=True,
     )
-
-    with st.sidebar:
-        selected = option_menu(
-            "Меню",
-            ["К Стёпе", 'Ментор', 'Карточки', 'Тестирование'],
-            icons=['controller', 'credit-card-2-front', 'bi-book', 'patch-question'],
-            menu_icon="cast",
-            default_index=1,
-        )
-
-    match selected:
-        case 'Игра':
-            game()
-        case 'Теория':
-            theory()
-        case 'Карточки':
-            cards()
-        case 'Тестирование':
-            exam()
-
-
-if __name__ == "__main__":
     main()
